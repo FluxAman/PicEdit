80a6fe030b70173daa1eb334f2a734ea8f1232ff
FluxAman <aman09@gmail.com>
2026-01-12 20:27:25 +0530

    Add Google site verification file for AdSense

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('google42a68fc9f9362179.html', views.google_verification, name='google_verification'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def google_verification(request):
+    return render(request, 'google42a68fc9f9362179.html', content_type='text/html')
